8ae96d42e6ebeff7dd9ee01efb7ca5c0ade439b9
HenryM975 <henry975@yandex.ru>
2021-05-24 21:24:59 +0300

    h,w

---
+++ 1st/1.1st.cpp
@@ -0,0 +1,6 @@
+#include "../include/std_lib_facilities.h"
+int main()
+{
+	cout << "Hello, prog\n Here we go\n";
+	return 0;
+}
---
+++ 1st/1.3st.cpp
@@ -0,0 +1,6 @@
+#include "../include/std_lib_facilities.h"
+int main()
+{
+	cout << "Blueberry buns!!!\n";
+	return 0;
+}

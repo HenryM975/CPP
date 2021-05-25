7783dc42c3681b86edd525bebce7dc50192cb93d
HenryM975 <henry975@yandex.ru>
2021-05-25 14:08:47 +0300

    cin

---
+++ 2nd/2.1.cpp
@@ -0,0 +1,8 @@
+#include "../include/std_lib_facilities.h"
+int main()
+{
+	cout << "введите имя (и нажмите 'enter'):\n";
+	string first_name;
+	cin >> first_name;
+	cout << "привет " << first_name << "!\n";
+}

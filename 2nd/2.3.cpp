a080d59c3880e695922abe2de30bde07f3ec110e
HenryM975 <henry975@yandex.ru>
2021-05-27 21:25:13 +0300

    +-*

---
+++ 2nd/2.3.cpp
@@ -0,0 +1,21 @@
+#include "../include/std_lib_facilities.h"
+int main()
+{
+	cout << "--------------------------------------\n введите целочисленн		ое значение и нажмите 'enter'\n";
+	int num;
+	cin >> num;
+	cout << "\nnum == " << num
+	     << "\nnum + num == "  << num + num
+       	     << "\nnum - 1 == " << num - 1
+	     << "\nnum * num == " << num * num
+	     << "\nnum / 2 == " << num / 2
+	     << "\nnum % 2 == " << num % 2
+	     << "\nnum++ == " << num++
+	     << "\nnum-- == " << num--
+	    //? << "\nnum += 1 == " << num += 1
+    	    //? << "\nnum -= 1 == " << num -= 1
+	    //? << "\nnum *= num == " << num *= num
+	    //? << "\nnum /= 2 == " << num /= 2
+	    //? << "\nnum %= 2 == " << num /= 2
+	     << "\n";	     
+}

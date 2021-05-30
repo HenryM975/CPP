26c3ff3a483e21ae78474d071590c4f676701935
HenryM975 <henry975@yandex.ru>
2021-05-30 23:38:28 +0300

    ++n

--- 2nd/2.4.cpp
+++ 2nd/2.4.cpp
@@ -3,10 +3,14 @@ int main()
 {
 	string previous = " ";
 	string current;
+	int number_of_words = 0;
 	 while(cin>>current)
 	{
+		++number_of_words;
 		if (previous == current)
-			cout << "Повторяющееся слово: " << current << "\n";
+			cout << "Повторяющееся слово: " << current
+			     << " его номер: "<< number_of_words
+		     	     << "\n";
 		previous = current;
 
 	}

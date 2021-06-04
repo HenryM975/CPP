1fa85956f4a866ec541e68a14b3a91f4af39d634
HenryM975 <henry975@yandex.ru>
2021-06-04 20:02:16 +0300

    ex4

---
+++ 2nd/2.8.cpp
@@ -0,0 +1,6 @@
+#include "../include/std_lib_facilities.h"
+int main()
+{
+	int q = 1, _q = 2;//, 2_q = 3;
+	cout << q << _q;
+}
---
+++ 2nd/2.9.cpp
@@ -0,0 +1,19 @@
+#include "../include/std_lib_facilities.h"
+int main()
+{
+        cout << "Введите 2 числа(через пробел):\n val1: ";
+        int val1, val2;
+        cin >> val1;
+	cout << " val2: ";
+	cin >> val2;
+        cout << "val1 == " << val1 << " val2 == " << val2 << "\n";
+	if(val1 > val2)
+		cout << " val1 > val2\n";
+	else if(val1 < val2)
+	       cout << " val1 < val2\n";
+	else
+		cout << " val1 == val2\n";
+	cout << "val1 + val2 == " << val1+val2 << "\n val1 - val2 == " << val1 - val2 << "\n val2 - val1 == " << val2 - val1 << "\n val1 * vl2 == " << val1 * val2 << "\n val1 / val2 == " << val1 / val2 << "\n val2 / val1 == " << val2 / val1 << "\n";
+
+}          
+

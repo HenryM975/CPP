d3a4cf4a7e6dfa309e45dfc5509fc6058d70f771
HenryM975 <henry975@yandex.ru>
2021-06-11 23:45:55 +0300

    ...

---
+++ 2nd/2.15.cpp
@@ -0,0 +1,10 @@
+#include "../include/std_lib_facilities.h"
+int main()
+{
+	cout << "Введите операцию и 2 числа:\n";
+	double num_1, num_2;
+	string operation;
+	cin >>  operation, num_1, num_2;
+	cout << operation << "|" << num_1 << "|"  << num_2;
+		
+}

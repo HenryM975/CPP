8dda97bff6cc6636177ec48fcb96a3ce3af62764
HenryM975 <henry975@yandex.ru>
2021-06-15 20:33:24 +0300

    e10

--- 2nd/2.15.cpp
+++ 2nd/2.15.cpp
@@ -2,9 +2,26 @@
 int main()
 {
 	cout << "Введите операцию и 2 числа:\n";
-	double num_1, num_2;
+	int  num_1, num_2;
 	string operation;
-	cin >>  operation, num_1, num_2;
-	cout << operation << "|" << num_1 << "|"  << num_2;
+	cin >>  operation >> num_1 >>  num_2;
+	if(operation == "-")
+		cout << num_1 - num_2;
+	else if(operation == "+")
+		cout << num_1 + num_2;
+	else if(operation == "*")
+		cout << num_1 * num_2;
+	else if(operation == "/")
+		cout << num_1 / num_2;
+	else if(operation== "%")
+		cout << num_1 % num_2;
+	else
+		cout << "error";
+	cout << "\n";
+			 
+
+
+
+		
 		
 }

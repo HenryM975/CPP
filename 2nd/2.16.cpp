af9c54b75ba5c7e076b6494b5d671c2473887c1c
HenryM975 <henry975@yandex.ru>
2021-06-17 21:53:00 +0300

    e11

--- 2nd/2.16.cpp
+++ 2nd/2.16.cpp
@@ -4,12 +4,23 @@ int main()
 	int current;
 	int mass[20];
 	int i = 0;
-	while (cin >> current)
+	while (cin >> current && current != 0)
 	{
 		mass[i] = current;
+		i++;
 	}
-	for(int j = 0; j != i; j++)
+	cout << "------------------------\n";
+	int j = 0;
+	int coin_1 = 5, coin1_sum = 0;
+	
+	while(i != j)
 	{
 		cout << mass[j] << "\n";
+		if(mass[j] == coin_1)
+			coin1_sum++;
+		j++;
+	
 	}
+	cout << "------------------------\n";
+	cout << "coin1_sum == " << coin1_sum << "\n";
 }

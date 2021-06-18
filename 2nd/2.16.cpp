aade70502c37471d03442da7c7b87c62a1783a9c
HenryM975 <henry975@yandex.ru>
2021-06-18 22:35:45 +0300

    ...

--- 2nd/2.16.cpp
+++ 2nd/2.16.cpp
@@ -19,8 +19,29 @@ int main()
 		if(mass[j] == coin_1)
 			coin1_sum++;
 		j++;
-	
+		
 	}
 	cout << "------------------------\n";
 	cout << "coin1_sum == " << coin1_sum << "\n";
+	/*
+	int k  = 0;
+	int part = 0;
+	while(k != i)
+	{
+		part = mass[k];
+		int l = 0;
+		int part_frequency = 0;
+		int no_repeat_list[20]; //исключить повторение в корневом цикле + установить выбор числа монет для определения размера массива на старте
+		while(l != i)
+		{
+			if(part == mass[l])
+				part_frequency++
+
+				
+			l++;
+		}
+		cout << mass[k] << " - " << part_frequency << "\n";
+		k++
+	}*/
+	
 }

029b736f5d7e370ddef86a898f8e079f7b18db4e
HenryM975 <henry975@yandex.ru>
2021-06-21 21:31:31 +0300

    if$

---
+++ 3rd/3.1.cpp
@@ -0,0 +1,20 @@
+#include "../include/std_lib_facilities.h"
+int main()
+{
+	constexpr double rub_dollar = 73.06;
+	constexpr double uan_dollar = 6.44;
+	constexpr double gri_dollar = 27.27;
+	cout << "Введите сумму и валюту(r(рубли)/u(юань)/g(гривна)):\n";
+	int sum = 0;
+	char currency;
+	cin >> sum >> currency;
+	if(currency == 'r')
+		cout << sum/rub_dollar << " $\n";
+	else if(currency == 'u')
+		cout << sum/uan_dollar << " $\n";
+	else if(currency == 'g')
+		cout << sum/gri_dollar << " $\n";
+	else
+		cout << "error\n";
+	
+}

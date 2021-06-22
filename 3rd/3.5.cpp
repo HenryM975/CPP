2ec1f7e229c9374573687723e4fa83743c7426b7
HenryM975 <henry975@yandex.ru>
2021-06-22 22:10:33 +0300

    A+1

---
+++ 3rd/3.5.cpp
@@ -0,0 +1,9 @@
+#include "../include/std_lib_facilities.h"
+int main()
+{
+	for(int i = 0; i < 100; i++)
+		cout << "a + " << i << " == " << 'a' + i << "\n";
+	for(int i = 0; i < 100; i++)
+		cout << "A + " << i << " == " << 'A' + i << "\n";
+	
+}

886e2f15eff0370ee7c7e9b5b3eb4530c0df0a2d
HenryM975 <henry975@yandex.ru>
2021-07-02 23:31:06 +0300

    ...

--- 3rd/3.10.cpp
+++ 3rd/3.10.cpp
@@ -1,15 +1,17 @@
 #include "../include/std_lib_facilities.h"
 int main()
 {
-	char data1, data2;
+	double data1, data2;
 	while(cin >> data1 && data1 != '|')
 	{
 		cin >> data2;
 		cout << data1 << "\t" << data2 << "\n";
-		if(data1 != data2)
+		if(data1 != data2 &&(data1 - data2 > 0.01 || data2 - data1 > 0.01))
 			cout << "Наибольшее значение равно " << (data1 > data2 ? data1 : data2) << "\nНаименьшее значение равно " << (data1 < data2 ? data1 : data2) << "\n"; 
 		else if(data1 == data2)
 			cout << "Числа равны\n";
+		else if(data1 - data2 < 0.01 || data2 - data1 < 0.01)
+			cout << "Числа почти равны\n";
 		else
 			cout << "ERROR\n";
 		

874be5a11f4d91c1fe8648db6a10c378ec009d26
HenryM975 <henry975@yandex.ru>
2021-07-17 21:22:23 +0300

    ...

--- 3rd/3.15.cpp
+++ 3rd/3.15.cpp
@@ -326,11 +326,272 @@ int main()
 		else if(answer == 'n')
 		{
 			//51 - 75
+			cout << "Число больше 62?(y/n)\n";
+			cin >> answer;
+			if(answer == 'y')
+			{
+				//63 - 75
+				cout << "Число больше 69?(y/n)\n";
+				cin >> answer;
+				if(answer == 'y')
+				{
+					//69 - 75
+					cout << "Число больше 72?(y/n)\n";
+					cin >> answer;
+					if(answer == 'y')
+					{
+						//73 - 75
+						cout << "Число больше 74?(y/n)\n";
+						cin >> answer;
+						if(answer == 'y')
+						{
+							//75!
+							cout << "Число 75!\n";
+						}
+						else if(answer == 'n')
+						{
+							//73 - 74
+							cout << "Число больше 73?(y/n)\n";
+							cin >> answer;
+							if(answer == 'y')
+							{
+								//74!
+								cout << "Число 74!\n";
+							}
+							else if(answer == 'n')
+							{
+								//73!
+								cout << "Число 73!\n";
+							}
+						}
+						else
+							cout << "Error\n";
+					}				}
+					else if(answer == 'n')
+					{
+						//69 - 72
+						cout << "Число больше 70?(y/n)\n";
+						cin >> answer;
+						if(answer == 'y')
+						{
+							//71 - 72
+							cout << "Число больше 71?(y/n)\n";
+							cin >> answer;
+							if(answer == 'y')
+							{
+								//72!
+								cout << "Число 72!\n";
+							}
+							else if(answer == 'n')
+							{
+								//71!
+								cout << "Число 71\n";
+							}
+							else
+								cout << "Error\n";
+						}
+						else if(answer == 'n')
+						{
+							//69 - 70
+							cout << "Число больше 69?(y/n)\n";
+							cin >> answer;
+							if(answer == 'y')
+							{
+								//70!
+								cout << "Число 70!\n";
+							}
+							else if(answer == 'n')
+							{
+								//69!
+								cout << "Число 69!\n";
+							}
+							else
+								cout << "Error\n";
+						}
+						else
+							cout << "Error\n";
+					}
+					else
+						cout << "Error\n";
+				}
+				else if(answer == 'n')
+				{
+					//63 - 69
+					cout << "Число больше 66?(y/n)\n";
+					cin >> answer;
+					if(answer == 'y')
+					{
+						//67 - 69
+						cout << "Число больше 68?(y/n)\n";
+						cin >> answer;
+						if(answer == 'y')
+						{
+							//69!
+							cout << "Число 69!\n";
+						}
+						else if(answer == 'n')
+						{
+							//67 - 68
+							cout << "Число больше 67?(y/n)\n";
+							cin >> answer;
+							if(answer == 'y')
+							{
+								//68!
+								cout << "Число 68!";
+							}
+							else if(answer == 'n')
+							{
+								//67!
+								cout << "Число 67!\n";
+							}
+							else
+								cout << "Error\n";
+						}
+						else 
+							cout << "Error\n";
+					}
+					else if(answer == 'n')
+					{
+						//63 - 66
+						cout << "Число больше 64?(y/n)\n";
+						cin >> answer;
+						if(answer == 'y')
+						{
+							//65 - 66
+							cout << "Число больше 65?(y/n)\n";
+							cin >> answer;
+							if(answer == 'y')
+							{
+								//66!
+								cout << "Число 66!\n";
+							}
+							else if(answer == 'n')
+							{
+								//65!
+								cout << "Число 65!\n";
+							}
+							else
+								cout << "Error\n";
+						}
+						else if(answer == 'n')
+						{
+							//63 - 64
+							cout << "Число больше 63?\n";
+							cin >> answer;
+							if(answer == 'y')
+							{
+								//64!
+								cout << "Число 64!\n";
+							}
+							else if(answer == 'n')
+							{
+								//63!
+								cout << "Число 63!\n";
+							}
+							else
+								cout << "Error\n";
+						}
+						else 
+							cout << "Error\n";
+					}
+					else 
+						cout << "Error\n";
+				}
+				else
+					cout << "Error\n";
+			}
+			else if(answer == 'n')
+			{
+				//51 - 62
+				cout << "Число больше 56?(y/n)\n";
+				cin >> answer;
+				if(answer == 'y')
+				{
+					//57 - 62
+					cout << "Число больше 59?(y/n)\n";
+					cin >> answer;
+					if(answer == 'y')
+					{
+						//60 - 62
+						cout << "Число больше 61?(y/n)\n";
+						cin >> answer;
+						if(answer == 'y')
+						{
+							//62!
+							cout << "Число 62!\n";
+						}
+						else if(answer == 'n')
+						{
+							//60 - 61
+							cout << "Число больше 60?(y/n)\n";
+							cin >> answer;
+							if(answer == 'y')
+							{
+								//61!
+								cout << "Число 61!\n";
+							}
+							else if(answer == 'n')
+							{
+								//60!
+								cout << "Число 60!\n";
+							}
+							else
+								cout << "Error!\n";
+						}
+						else 
+							cout << "Error\n";
+					}
+					else if(answer == 'n')
+					{
+						//57 - 59
+						cout << "Число больше 58?\n";
+						cin >> answer;
+						if(answer == 'y')
+						{
+							//59!
+							cout << "Число 59!\n";
+						}
+						else if(answer == 'n')
+						{
+							//57 - 58
+							cout << "Число больше 57?(y/n)";
+							cin >> answer;
+							if(answer == 'y')
+							{
+								//58!
+								cout << "Число 58!\n";
+							}
+							else if(answer == 'n')
+							{
+								//57!
+								cout << "Число 57!\n";
+							}
+							else
+								cout << "Error\n";
+
+						}
+						else
+							cout << "Error\n";
+					}
+					else
+						cout << "Error\n";
+
+				}
+				else if(answer == 'n')
+				{
+					//51-56
+				}
+				else
+					cout << "Error\n";
+			}
+			else
+				cout << "Error\n";
 		}
 		else
 			cout << "Error\n";
 
 	}
 
+	
 		
-}
+
---
+++ 3rd/3.20.cpp
@@ -0,0 +1,37 @@
+#include "../include/std_lib_facilities.h"
+
+int main()
+{
+	int answer;
+	cout << "Выберите способ ввода чисел: буквенный (1), числовой(2)\n";
+	cin >> answer;
+	if(answer == 1)
+	{
+		string <vector> vec = {"zero", "one", "two", "three", "four","five", "six", "seven", "eight", "nine", "ten"}
+		string number1, number2;
+		char oper;
+		cout << "Введите 2 числа в буквенном виде и операцию (+-*/)\n";
+	//...
+	}
+	else if(answer == 2)
+	{
+	double num1, num2;
+	char operation;
+	cout << "Введите 2 числа и операцию (+-*/)\n";
+	while(cin >> num1 >> num2 >> operation)
+	{
+		if(operation == '+')
+			cout << num1 << " + " << num2 << " == " << num1 + num2 << "\n";
+		else if(operation == '-')
+			cout << num1 << " - " << num2 << " == " << num1 - num2 << "\n";
+		else if(operation == '*')
+			cout << num1 << " * " << num2 << " == " << num1 * num2 << "\n";
+		else if(operation == '/')
+			cout << num1 << " / " << num2 << " == " << num1 / num2 << "\n";
+		else
+			cout << "Error\n";
+	}
+	}
+	else
+		cout << "Error\n";
+}

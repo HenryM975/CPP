5e6126cbe63d4afe165c8183cf89eaebc8eb9e73
HenryM975 <henry975@yandex.ru>
2021-07-18 14:40:12 +0300

    ..

--- 3rd/3.20.cpp
+++ 3rd/3.20.cpp
@@ -7,11 +7,28 @@ int main()
 	cin >> answer;
 	if(answer == 1)
 	{
-		string <vector> vec = {"zero", "one", "two", "three", "four","five", "six", "seven", "eight", "nine", "ten"}
+		vector<string> vec = {"zero", "one", "two", "three", "four","five", "six", "seven", "eight", "nine", "ten"};
 		string number1, number2;
 		char oper;
+		double number1_result, number2_result;
 		cout << "Введите 2 числа в буквенном виде и операцию (+-*/)\n";
-	//...
+		while(cin >> number1 >> number2 >> oper)
+		{
+			int i = 0;
+			for(;i < 11; ++i)
+			{
+				if(number1 == vec[i])
+				{
+					//...
+				}
+				if(number2 == vec[i])
+				{
+					//...
+				}
+			}	
+		}
+			
+
 	}
 	else if(answer == 2)
 	{

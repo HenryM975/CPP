a21c95e87983f2601640d06923cef574c57ea455
HenryM975 <henry975@yandex.ru>
2021-07-19 14:49:12 +0300

    ...

--- 3rd/3.21.cpp
+++ 3rd/3.21.cpp
@@ -2,10 +2,23 @@
 
 int main()
 {
-	double num = 1;  //int
-	for(int i = 0; i < 64; i++)
+	unsigned long long int limit = 0;
+	cout << "Введите предельное значение\n";
+	cin >> limit;
+
+
+	unsigned long long int  num = 1;  //int
+	int i = 0;
+	vector <unsigned long long int> vec;
+	for(; i < 64; ++i)
 	{
 		num *= 2;
-		cout << num <<"\n";
+		/*if(num >= limit)
+			cout << "Limit!\n";*/
+		vec.push_back(num);
+		cout << i << " - " <<  num <<"\n";
+		for(int j = 0; j < i; j++)
+			cout << vec[j] << " ";
+		cout << "\n\n";
 	}
 }

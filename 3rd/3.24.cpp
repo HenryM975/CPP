f36cfcfc793d798fc20a22f507c0e4b6b476e99a
HenryM975 <henry975@yandex.ru>
2021-07-24 18:02:40 +0300

    e11

---
+++ 3rd/3.24.cpp
@@ -0,0 +1,27 @@
+#include "../include/std_lib_facilities.h"
+
+int main()
+{
+	vector <int> all_nums;
+	vector <int> primes;
+	int i = 1;
+	for(; i < 100; ++i)
+	{
+		all_nums.push_back(i);	
+		int dividers = 0;	
+		for(int j = 0; j < i; ++j)
+		{
+			if(i % all_nums[j] == 0)
+			{
+				++dividers;
+			}
+		}
+		if(dividers <= 2)
+			primes.push_back(i);
+	}
+	int k = 0;
+	for(; k < primes.size(); ++k)
+		cout << primes[k] << " ";
+	cout << "\n";
+
+}

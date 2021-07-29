293a2fb9bc1e1bdc4d73388a5d757741186b9898
HenryM975 <henry975@yandex.ru>
2021-07-29 14:59:03 +0300

    ...

--- 3rd/3.26.cpp
+++ 3rd/3.26.cpp
@@ -11,4 +11,16 @@ for(int i = 0; i < limit; ++i)
 	values.push_back(i);
 	cout << i << " ";
 }
+//vec.erase(vec.begin() + 1)
+/*for(int i = 0; i < limit; ++i)
+{
+	if(i % 2 == 0)
+		values.erase(values.begin());
+}*/
+values.erase(values.cbegin() + 1);
+for(int i = 0; i < limit - 1; ++i)
+{
+	cout << values[i];
 }
+}
+	

b0fc9a9dad0f6b283e191e393e30175474b83f2d
HenryM975 <henry975@yandex.ru>
2021-08-03 15:02:06 +0300

    e13

--- 3rd/3.26.cpp
+++ 3rd/3.26.cpp
@@ -8,13 +8,14 @@ cout << "Введите крайнее значение:\n";
 cin >> limit;
 for(int i = 0; i < limit + 1; i++)
 	{
-		values[i] = i;
+		values.push_back(i);
 	}
+
 for(int p = 2; p < limit + 1; p++)
 	{
 		if (values[p] != 0)
 		{
-			cout << values[p];
+			cout << values[p] << " ";
 			for(int j = p * p; j < limit + 1; j += p)
 				values[j] = 0;
 		}

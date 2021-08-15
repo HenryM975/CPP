a015911f9f5566cf27417c5c28b0acdc3b1b0bc1
HenryM975 <henry975@yandex.ru>
2021-08-15 04:37:35 +0300

    e15

--- 3rd/3.27.cpp
+++ 3rd/3.27.cpp
@@ -2,29 +2,33 @@
 //решето эратосфена
 int main()
 {
-int limit, sum_of_nums, i = 0;
-vector <int> values;
+int limit, sum_of_nums;
+vector <int> values, final_vector;
 cout << "Введите необходимое количество простых чисел\n";
 cin >> sum_of_nums;
-limit = 100;
+if(sum_of_nums > 4700)
+	cout << "Too big number!\n";
+else
+{
+limit = sum_of_nums * 10;
 for(int i = 0; i < limit + 1; i++)
         {
                 values.push_back(i);
         }
 
-int p = 2;
-//while(vector.size != sum of nums)
-for(int p = 2; p < limit + 1; p++)
+//while(final_vector.size() != sum of nums)
+for(int p = 2; final_vector.size() != sum_of_nums; p++)
         {
-		++p;
                 if (values[p] != 0)
                 {
-                        cout << values[p] << " ";
+			final_vector.push_back(values[p]);
+                        cout << values[p] << "-" << final_vector.size() << " ";
                         for(int j = p * p; j < limit + 1; j += p)
                                 values[j] = 0;
                 }
         }
 
-
+cout << "\n";
+}
 }
 

8eea6766e421727580d8ef0ee2ef66407298175c
HenryM975 <henry975@yandex.ru>
2021-09-01 04:14:35 +0300

    ...

---
+++ 3rd/3.31.cpp
@@ -0,0 +1,18 @@
+#include "../include/std_lib_facilities.h"
+
+int main()
+{
+	vector <int> ages;
+	int age, a = 0,   n = 0;
+	vector <string> names;
+	string name;
+	cout << "age" << " name";
+	cin >> age >> name;
+	for(int a2 = 0; a2 < a; a2++)
+	ages.push_back(age)
+	++a;
+	names.push_back(name)
+	++n;
+	
+	
+}

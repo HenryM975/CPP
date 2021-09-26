415642c234ed15142bb92a26fe6858081d2822f2
HenryM975 <henry975@yandex.ru>
2021-09-26 15:33:22 +0300

    ...

---
+++ 4th/4.2.cpp
@@ -0,0 +1,24 @@
+#include "../include/std_lib_facilities.h"
+
+int main()
+{
+	vector <double> temps;
+	double sum = 0;
+	double high_temp = -1000;
+	double low_temp = 1000;
+	int no_of_temps = 0;
+	for (double temp; cin >> temp; )
+	{
+		++no_of_temps;
+		sum += temp;
+		if(temp  > high_temp) high_temp = temp;
+		if(temp < low_temp) low_temp = temp;
+	}
+	cout << "Максимальная температура: "
+		<< high_temp << endl;
+	cout << "Минимальная температура: "
+		<< low_temp << endl;
+	cout << "Средняя температура: "
+		<< sum/no_of_temps << endl;
+
+}

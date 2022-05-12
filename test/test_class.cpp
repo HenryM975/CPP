12b6299d8f28c1172d4aa4352b289c512a0136f9
HenryM975 <henry975@yandex.ru>
2022-05-12 15:58:52 +0300

    ...

--- test/test_class.cpp
+++ test/test_class.cpp
@@ -18,7 +18,8 @@ int main()
 	Class1.num2 = 2;
 
 	std::cout << Class1.num1 << "|" << Class1.num2 << "|" << Class1.Func(1, 2) << std::endl;
-
+	
+	//создание массива объектов
 	TestClass t[10];
 	t[0].num1 = 0;
 	t[0].num2 = 0;
@@ -28,11 +29,13 @@ int main()
 
 	std::cout << t[0].num1 << "|" << t[0].num2 << "|" << t[1].num1 <<  "|" << t[1].num2 << std::endl;
 	
+	//создание указателя на класс / объект класса
 	TestClass* pT;
 	TestClass* pClass1;
 	pClass1 = &Class1;
-
-	std::cout << pT << "|" << pClass1 << "|" << ++pClass1 << "|" << pClass1->num1 << "|" << pClass1->num2 << std::endl;
+	
+	//способы указать на хронящийся элемент по указателю
+	std::cout << pT << "|" << pClass1 << "|" << pClass1 << "|" << pClass1->num1 << "|" << pClass1->num2 << "||" << (*pClass1).num1<< "|" << (*pClass1).num2 << std::endl;
 
 
 
---
+++ test/test_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//#include <string>
+
+
+int main()
+{
+	std::string strVal;
+	std::cin >> strVal;
+	std::cout << strVal;
+	return 0;
+}

a3a466dc8698ab7a4a69087af952c4df21df6f80
HenryM975 <henry975@yandex.ru>
2022-05-16 20:16:53 +0300

    start constr

--- test/test_class.cpp
+++ test/test_class.cpp
@@ -2,13 +2,21 @@
 
 class TestClass
 {
+	friend void testFriendFunc(TestClass *);//?
 	public:
+		TestClass()
+		{
+			std::cout << "Конструируем TestClass" << std::endl;
+			int constrNum = 12;//?
+		}
 		int Func(int smth1, int smth2)
 		{
 			return smth1 + smth2;
 		}
 		int num1;
 		int num2;
+	protected:
+		int protectedNum3;
 };
 
 int main()
@@ -17,7 +25,7 @@ int main()
 	Class1.num1 = 1;
 	Class1.num2 = 2;
 
-	std::cout << Class1.num1 << "|" << Class1.num2 << "|" << Class1.Func(1, 2) << std::endl;
+	std::cout << Class1.num1 << "|" << Class1.num2 << "|" << Class1.Func(1, 2) /*<< "Class1.constrNum:" << Class1.constrNum */<< std::endl;
 	
 	//создание массива объектов
 	TestClass t[10];
@@ -36,7 +44,9 @@ int main()
 	
 	//способы указать на хронящийся элемент по указателю
 	std::cout << pT << "|" << pClass1 << "|" << pClass1 << "|" << pClass1->num1 << "|" << pClass1->num2 << "||" << (*pClass1).num1<< "|" << (*pClass1).num2 << std::endl;
-
+	/*testFriendFunc(TestClass  *Class1)//?
+	{
+	}*/
 
 
 	return 0;
